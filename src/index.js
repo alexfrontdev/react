e33e500311ac6e4197683d273fc9130856b8d07c
alexfrontdev <alexand.front.dev@gmail.com>
2021-05-10 16:56:40 +0400

    lesson number2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import MyApp from './App';
+
+
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
